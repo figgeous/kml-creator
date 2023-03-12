9c08c2ee2e9c0f853aa35de72737f60138297ad8
figgeous <thomas.oneill@gmail.com>
2023-03-12 21:18:57 +0100

    Polygon experimenting

--- scripts/gdal_scripts.py
+++ scripts/gdal_scripts.py
@@ -1,24 +1,13 @@
+from osgeo import gdalconst,ogr,gdal,gdal_array
 import os
-
-from osgeo import gdal
-from osgeo import gdal_array
-from osgeo import gdalconst
-from osgeo import ogr
-from osgeo_utils.gdal_polygonize import gdal_polygonize
-
 from constants import *
 
+def gdal_open_tif(*,tif_name:str):
+    return gdal.Open(TIF_PATH+tif_name+".tif", gdal.GA_ReadOnly)
 
-def gdal_open_tif(*, tif_name: str):
-    assert ".tif" not in tif_name
-    return gdal.Open(TIF_PATH + tif_name + ".tif", gdal.GA_ReadOnly)
-
-
-def gdal_print_metadata(*, tif_name: str) -> None:
-    assert ".tif" not in tif_name
-    metadata = os.popen("gdalinfo " + TIF_PATH + tif_name + ".tif").read()
+def gdal_print_metadata(*,tif_name:str) -> None:
+    metadata = os.popen("gdalinfo "+TIF_PATH+tif_name+".tif").read()
     print(metadata)
-
 
 def gdal_print_first_feature(file):
     """
@@ -26,40 +15,37 @@ def gdal_print_first_feature(file):
     {"type": "Feature", "geometry": {"type": "Point", "coordinates": [8.943549, 56.996942]}, "properties": {"bm_dens": 10, "bm_size": 58}, "id": 0}
     """
     shape = file.GetLayer(0)
-    # first feature of the shapefile
+    #first feature of the shapefile
     feature = shape.GetFeature(1)
     first = feature.ExportToJson()
-    print(first)  # (GeoJSON format)
-
-
-def gdal_open_shp(*, shp_name: str):
+    print(first) # (GeoJSON format)
+    
+def gdal_open_shp(*,shp_name:str):
     assert ".shp.zip" not in shp_name
-    return ogr.Open(SHP_PATH + shp_name + ".shp.zip")
-
+    return ogr.Open(SHP_PATH+shp_name+".shp.zip")
 
 def gdal_run_interpolation(
     *,
     # For gdal_grid
-    input_shp_name: str,
-    target_column: str,
-    output_tif_name: str,
+    input_shp_name:str,
+    target_column:str,
+    output_tif_name:str,
     # Below are associated with GdalOptions
-    output_format: str = "Gtiff",
+    output_format:str="Gtiff",
     output_type="Byte",
-    width: int = 0,
-    height: int = 0,
-    output_res: list = None,
-    outputBounds: list = None,
-    algorithm: str = "invdist",
-    power: int = None,
-    smoothing: float = None,
-    radius1: float = None,
-    radius2: float = None,
-    angle: int = None,
-    max_points: int = 0,
-    min_points: int = 0,
-    nodata: float = None,
-) -> str:
+    width:int=0, height:int=0,
+    output_res:list = None,
+    outputBounds:list = None,
+    algorithm:str="invdist",
+    power:int=1,
+    smoothing:float=None,
+    radius1:float=None,
+    radius2:float=None,
+    angle:int=None,
+    max_points:int=0,
+    min_points:int=0,
+    nodata:float=None,
+    ) -> str:
     """
     Keyword arguments are :
         input_shp_name ---
@@ -90,15 +76,9 @@ def gdal_run_interpolation(
     """
     assert ".shp.zip" not in input_shp_name
     assert ".tif" not in output_tif_name
-
+    
     def _get_output_bounds() -> list:
-        return [
-            "%.18g" % outputBounds[0],
-            "%.18g" % outputBounds[2],
-            "-tye",
-            "%" % outputBounds[1],
-            "%" % outputBounds[3],
-        ]
+        return ['%.18g' % outputBounds[0], '%.18g' % outputBounds[2], '-tye', '%.18g' % outputBounds[1], '%.18g' % outputBounds[3]]
 
     def _get_algorithm_str() -> str:
         s = f"{algorithm}:"
@@ -111,56 +91,57 @@ def gdal_run_interpolation(
         s += f"min_points={min_points}:" if min_points else ""
         s += f"nodata={nodata}:" if nodata else ""
         return s
-
+    
     # Not implemented settings include: creationOptions, layers, SQLStatement, z_increase, z_multiply
     new_options = []
     if output_format is not None:
-        new_options += ["-of", output_format]
+        new_options += ['-of', output_format]
     if output_type is not None:
-        new_options += ["-ot", output_type]
+        new_options += ['-ot', output_type]
     if width != 0 or height != 0:
-        new_options += ["-outsize", str(width), str(height)]
+        new_options += ['-outsize', str(width), str(height)]
     if outputBounds is not None:
-        new_options += ["-txe"] + _get_output_bounds()
+        new_options += ['-txe']+_get_output_bounds()
     # Maybe include outputSRS later?
     # if outputSRS is not None:
     #     new_options += ['-a_srs', str(outputSRS)]
     if algorithm is not None:
-        new_options += ["-a", _get_algorithm_str()]
+        new_options += ['-a', _get_algorithm_str()]
     if target_column is not None:
-        new_options += ["-zfield", target_column]
-    # Maybe include spatFilter later?
+        new_options += ['-zfield', target_column]
+    #Maybe include spatFilter later?
     # if spatFilter is not None:
     #     new_options += ['-spat', str(spatFilter[0]), str(spatFilter[1]), str(spatFilter[2]), str(spatFilter[3])]
     if output_res is not None:
-        new_options += ["-tr", str(output_res[0]), str(output_res[1])]
-    print("Options: ", new_options)
-
-    grid_options = gdal.GridOptions(options=new_options)
+        new_options += ['-tr', str(output_res[0]), str(output_res[1])]
+    print("Options: ",new_options)
+    
+    grid_options = gdal.GridOptions(
+        options=new_options
+    ) 
 
     output_tif_name += f"-{algorithm}-{power}-{smoothing}-{radius1}-{radius2}-{angle}-{max_points}-{min_points}"
-    dest_name = TIF_PATH + output_tif_name + ".tif"
-    src_ds = SHP_PATH + input_shp_name + ".shp.zip"
-
-    print("Running interpolation on: " + src_ds)
-    print("Saving to: " + dest_name)
-    idw = gdal.Grid(destName=dest_name, srcDS=src_ds, options=grid_options)
+    dest_name = TIF_PATH+output_tif_name+".tif"
+    src_ds = SHP_PATH+input_shp_name+".shp.zip"
+    
+    print("Running interpolation on: "+src_ds)
+    print("Saving to: "+dest_name)
+    idw = gdal.Grid(
+        destName=dest_name,
+        srcDS=src_ds,
+        options=grid_options)
     idw = None
     return output_tif_name
-
-
-def plot_raster(*, tif_name: str) -> None:
+    
+def plot_raster(*,tif_name:str) -> None:
     import numpy as np
     import matplotlib.pyplot as plt
-
     dataset = gdal_open_tif(tif_name=tif_name)
     # Allocate our array using the first band's datatype
     image_datatype = dataset.GetRasterBand(1).DataType
 
-    image = np.zeros(
-        (dataset.RasterYSize, dataset.RasterXSize, dataset.RasterCount),
-        dtype=gdal_array.GDALTypeCodeToNumericTypeCode(image_datatype),
-    )
+    image = np.zeros((dataset.RasterYSize, dataset.RasterXSize, dataset.RasterCount),
+                    dtype=gdal_array.GDALTypeCodeToNumericTypeCode(image_datatype))
     # Loop over all bands in dataset
     for b in range(dataset.RasterCount):
         # Remember, GDAL index is on 1, but Python is on 0 -- so we add 1 for our GDAL calls
@@ -168,55 +149,36 @@ def plot_raster(*, tif_name: str) -> None:
         # Read in the band's data into the third dimension of our array
         image[:, :, b] = band.ReadAsArray()
 
-    plt.imshow(image[:, :, 0], origin="lower")
+    plt.imshow(image[:, :, 0], origin='lower')
     plt.colorbar()
 
-
-def get_tif_array(*, tif_name: str):
-    import numpy as np
-    import matplotlib.pyplot as plt
-
-    dataset = gdal_open_tif(tif_name=tif_name)
-    # Allocate our array using the first band's datatype
-    image_datatype = dataset.GetRasterBand(1).DataType
-
-    image = np.zeros(
-        (dataset.RasterYSize, dataset.RasterXSize, dataset.RasterCount),
-        dtype=gdal_array.GDALTypeCodeToNumericTypeCode(image_datatype),
-    )
-    # Loop over all bands in dataset
-    for b in range(dataset.RasterCount):
-        # Remember, GDAL index is on 1, but Python is on 0 -- so we add 1 for our GDAL calls
-        band = dataset.GetRasterBand(b + 1)
-        # Read in the band's data into the third dimension of our array
-        image[:, :, b] = band.ReadAsArray()
-
-    return image
-
-
 def make_polygons(
-    input_tif: str,
-    input_band: int = 1,
-    output_shp: str = None,
-    output_layer: str = None,
-    output_field: str = None,
-    connectedness8: bool = False,
-    options: str = None,
-) -> None:
-    assert ".tif" not in input_tif
-    assert ".shp" not in output_shp
+        input_tif:str,
+        input_band:int=1,
+        output_shp:str=None,
+        output_layer:str=None,
+        output_field:str=None,
+        mask:str='default',
+        connectedness8:bool=False,
+        options:list=None,
+        ) -> None:
+    assert '.tif' not in input_tif
+    if output_shp:
+        assert '.shp' not in output_shp
+    from osgeo_utils.gdal_polygonize import gdal_polygonize
+    input_tif_path = TIF_PATH+input_tif+".tif" if input_tif else None
 
-    input_tif = TIF_PATH + input_tif + ".tif" if input_tif else None
-    output_shp = SHP_PATH + output_shp + ".shp.zip" if output_shp else None
+    if output_shp:
+        output_shp_path = SHP_PATH+output_shp+".shp.zip"
+    else:
+        output_shp_path = SHP_PATH+input_tif+".shp.zip"
 
-    src_ds = TIF_PATH + "sample.tif"
-    dst_filename = SHP_PATH + "sample.shp.zip"
     gdal_polygonize(
-        src_filename=input_tif,
-        dst_filename=output_shp,
+        src_filename=input_tif_path,
+        dst_filename=output_shp_path,
         band_number=input_band,
         dst_layername=output_layer,
         dst_fieldname=output_field,
+        mask=mask,
         connectedness8=connectedness8,
-        options=options,
-    )
+        options=options)
--- unimportant/github_gdal_ex.py
+++ unimportant/github_gdal_ex.py
@@ -1,16 +1,16 @@
-# This file is a copy paste from stackoverflow. We might be able to get some ideas from it.
-import os
+#This file is a copy paste from stackoverflow. We might be able to get some ideas from it.
 
 from osgeo import gdal
+import os
 
-shapefiles = [folder + "/" + file for file in os.listdir(folder) if "shp" in file]
-output = "output"
+shapefiles = [folder + '/' + file for file in os.listdir(folder) if 'shp' in file]
+output = 'output'
 if not os.path.exists(output):
     os.mkdir(output)
 RefImage = ref_image
-gdalformat = "GTiff"
+gdalformat = 'GTiff'
 datatype = gdal.GDT_Byte
-burnVal = 1  # value for the output image pixels
+burnVal = 1 #value for the output image pixels
 
 # Get projection info from reference image
 RasImage = gdal.Open(RefImage, gdal.GA_ReadOnly)
@@ -18,33 +18,26 @@ RasImage = gdal.Open(RefImage, gdal.GA_ReadOnly)
 for Shapefile in shapefiles:
     ds = ogr.Open(Shapefile)
     ds_layer = ds.GetLayer()
-
-
 # convert a shapefile to raster
 def convertShpToRaster(inputfile, band, size):
     shp = ogr.Open(inputfile)
     lyr = shp.GetLayer(0)
     lyrname = lyr.GetName()
-    # color = plt.gray()
+    #color = plt.gray()
 
-    subprocess.call(
-        "gdal_rasterize -ts {0} {1} -l {2} -burn {3} ./{4} ./{5}.tif".format(
-            size[0], size[1], lyrname, band, inputfile, output + "/" + lyrname
-        )
-    )
-
+    subprocess.call('gdal_rasterize -ts {0} {1} -l {2} -burn {3} ./{4} ./{5}.tif' \
+                .format(size[0], size[1], lyrname, band, inputfile, output + '/' + lyrname))
 
 def inputToShapefile():
     # input color, resolution and shapefile location
-    # color = plt.gray()
+    #color = plt.gray()
     band = RasImage.GetRasterBand(1)
     size = RasImage.RasterXSize, RasImage.RasterYSize
 
     # call the function
     for inputfile in shapefiles:
-        convertShpToRaster(inputfile, band, size)
-
+        convertShpToRaster(inputfile, band, size)   
 
 inputToShapefile()
 
-gdal.Polygonize(srcband, None, dst_layer, -1, [], callback=None)
+gdal.Polygonize( srcband, None, dst_layer, -1, [], callback=None )
